2fceea32bae2b6fd738c158dcb62de8bab76c9eb
voger <voger@mylaptop.com>
2018-02-21 21:30:51 +0200

    Add accounts context

---
+++ lib/gqlardian/accounts/user.ex
@@ -0,0 +1,20 @@
+defmodule GQLardian.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias GQLardian.Accounts.User
+
+
+  schema "users" do
+    field :password_hash, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%User{} = user, attrs) do
+    user
+    |> cast(attrs, [:username, :password_hash])
+    |> validate_required([:username, :password_hash])
+  end
+end
