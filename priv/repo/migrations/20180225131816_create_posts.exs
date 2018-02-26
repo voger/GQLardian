a54687f50b7d6942aef38b12588e54c53f6c0ee6
voger <voger@mylaptop.com>
2018-02-26 23:30:56 +0200

    foreign keys have a suffix _id

--- priv/repo/migrations/20180225131816_create_posts.exs
+++ priv/repo/migrations/20180225131816_create_posts.exs
@@ -5,10 +5,10 @@ defmodule GQLardian.Repo.Migrations.CreatePosts do
     create table(:posts) do
       add :title, :string, null: false
       add :content, :text, null: false
-      add :author, references(:users, on_delete: :delete_all, on_update: :update_all), null: false
+      add :author_id, references(:users, on_delete: :delete_all, on_update: :update_all), null: false
 
-      add :status,
-          references(:post_statuses, on_update: :update_all, column: :status, type: :citext), null: false
+      add :status_id,
+          references(:post_statuses, default: "draft", on_update: :update_all, column: :status, type: :citext), null: false
 
       timestamps()
     end
