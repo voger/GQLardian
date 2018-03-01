e6a65b27eac2cd3907dcfaed3d76e7cf15aaabe0
voger <voger@mylaptop.com>
2018-03-01 21:57:24 +0200

    Move import_types in schema.ex

--- lib/gqlardian_web/schema.ex
+++ lib/gqlardian_web/schema.ex
@@ -2,6 +2,10 @@ defmodule GQLardianWeb.Schema do
   use Absinthe.Schema
 
   import_types __MODULE__.UserTypes
+  import_types __MODULE__.PostTypes
+  import_types __MODULE__.ValidationMessageTypes
+
+  import_types Absinthe.Type.Custom
 
   query do
     import_fields :user_queries
@@ -9,5 +13,6 @@ defmodule GQLardianWeb.Schema do
 
   mutation do
     import_fields :user_mutations
+    import_fields :post_mutations
   end
 end
