34f94ac4b0e3435c964271fc1939960bdb3c6b5a
voger <voger@mylaptop.com>
2018-03-04 16:17:39 +0200

    we can create and query posts

--- lib/gqlardian/posts/post.ex
+++ lib/gqlardian/posts/post.ex
@@ -4,23 +4,20 @@ defmodule GQLardian.Posts.Post do
   import Ecto.Changeset
   alias GQLardian.Posts.{Post, PostStatus}
 
-
-
   schema "posts" do
     field :title, :string
     field :content, :string
 
-    belongs_to :status, PostStatus, references: :status_id, type: :string
+    belongs_to :status, PostStatus, references: :status, type: :string
     belongs_to :author, GQLardian.Accounts.User
     timestamps()
   end
 
   @doc false
   def create_changeset(%Post{} = post, attrs) do
+    # Temporary
     post
     |> cast(attrs, [:title, :content])
-
-    # Temporary
     |> put_change(:author_id, 1)
   end
 end
