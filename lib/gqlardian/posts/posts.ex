9391ef92bdaa9eecbea0f27d6ea9fcac7a7b77e3
voger <voger@mylaptop.com>
2018-03-07 23:11:54 +0200

    add updatePost in graphql

--- lib/gqlardian/posts/posts.ex
+++ lib/gqlardian/posts/posts.ex
@@ -3,17 +3,17 @@ defmodule GQLardian.Posts do
   The Posts context
   """
 
-
   alias GQLardian.Repo
   alias GQLardian.Posts.Post
 
   def create_post(attrs) do
     %Post{}
     |> Post.create_changeset(attrs)
-    |> Repo.insert()
+    |> Repo.insert!()
   end
 
   def update_post(post_id, attrs \\ %{}) do
+    # |> Cl.inspect(label: "-b updated changeset")
     post_id
     |> get_post()
     |> Post.update_changeset(attrs)
--- lib/gqlardian_web/resolvers/posts.ex
+++ lib/gqlardian_web/resolvers/posts.ex
@@ -5,16 +5,15 @@ defmodule GQLardianWeb.Resolvers.Posts do
   import Absinthe.Resolution.Helpers, only: [on_load: 2]
 
   def create_post(_, %{input: arguments}, _res) do
-    with {:ok, %Post{} = post} <- Posts.create_post(arguments) do
-      {:ok, post}
-    else
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:ok, changeset}
-    end
+    {:ok,Posts.create_post(arguments)}
   end
 
   def get_post(_, %{id: id}, _res) do
     {:ok, Posts.get_post(id)}
+  end
+
+  def update_post(_, %{input: arguments}, _res) do
+    {:ok, Posts.update_post(arguments.id, arguments)}
   end
 
   def posts(_, _, _) do
