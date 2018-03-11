20a5a60b51918c21484e944158522323bf77ac59
voger <voger@mylaptop.com>
2018-03-11 11:51:07 +0200

    Some initial tests
    Create post tests don't work because there is no functionality yet
    to identify the user

---
+++ test/gqlardian_web/schema/mutation/user.exs
@@ -0,0 +1,66 @@
+defmodule GQLardianWeb.Schema.Mutation.UserMutationsTest do
+  use GQLardianWeb.ConnCase, async: true
+
+  @query """
+  mutation CreateUser($input: CreateUserInput!){
+    createUser(input: $input){
+      messages {
+        code
+        field
+        message
+      },
+      result {
+        id
+        username
+      },
+      successful
+    }
+  }
+  """
+  @variables """
+  {
+    "input": {
+      "password": "hunter2",
+      "username": "AzureDiamond"
+    }
+  }
+  """
+  test "create user creates a user" do
+    conn = post(build_conn(), "/api", query: @query, variables: @variables)
+
+    assert %{
+             "data" => %{
+               "createUser" => %{
+                 "successful" => true,
+                 "result" => %{
+                   "username" => "AzureDiamond",
+                   "id" => _
+                 },
+                 "messages" => []
+               }
+             }
+           } = json_response(conn, 200)
+  end
+
+  test "create user with the same username fails with error" do
+    GQLardian.Accounts.create_user(%{username: "AzureDiamond", password: "hunter2"})
+
+    conn = post(build_conn(), "/api", query: @query, variables: @variables)
+
+    assert %{
+             "data" => %{
+               "createUser" => %{
+                 "successful" => false,
+                 "result" => nil,
+                 "messages" => [
+                   %{
+                     "message" => "already taken",
+                     "field" => "username",
+                     "code" => "unknown"
+                   }
+                 ]
+               }
+             }
+           } = json_response(conn, 200)
+  end
+end
