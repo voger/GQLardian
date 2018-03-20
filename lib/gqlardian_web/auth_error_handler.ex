85ccea80990f3dce2b9923e24ec308260129a116
voger <voger@mylaptop.com>
2018-03-20 21:02:34 +0200

    Introduce credo and do the checks

--- lib/gqlardian_web/auth_error_handler.ex
+++ lib/gqlardian_web/auth_error_handler.ex
@@ -1,10 +1,7 @@
 defmodule GQLardianWeb.AuthErrorHandler do
   import Plug.Conn
-  require Cl
 
   def auth_error(conn, {type, reason}, _opts) do
-    
-    Cl.inspect({type, reason}, label: "-b {type, reason}")
     body = Poison.encode!(%{message: to_string(type)})
     send_resp(conn, 401, body)
   end
